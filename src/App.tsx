aa376f1bb75ff4613d3ed366cc13803e82af2956
Léonard Wojcik <leonardw@tutanota.com>
2022-06-18 07:59:01 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <p>Rien ne sert de courir si l'on n'est pas pressé.</p>
+    </div>
+  )
+}
+
+export default App
