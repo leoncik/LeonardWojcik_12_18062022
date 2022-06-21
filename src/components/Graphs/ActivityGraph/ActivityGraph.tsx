0fb32e5ce4a40eea9b431186c8da63c39f5329cc
Léonard Wojcik <leonardw@tutanota.com>
2022-06-21 20:10:02 +0200

    feat: add second bar to ActivityGraph - Add weight bar to graph

--- src/components/Graphs/ActivityGraph/ActivityGraph.tsx
+++ src/components/Graphs/ActivityGraph/ActivityGraph.tsx
@@ -1,5 +1,5 @@
 import { useRef, useEffect } from 'react';
-import { select } from 'd3-selection';
+import { select } from 'd3';
 import { MOCKED_DATA } from '../../../helpers/MOCKED_DATA';
 
 function ActivityGraph() {
@@ -11,74 +11,61 @@ function ActivityGraph() {
     const testRef = useRef<SVGSVGElement | null>(null);
 
     const svgHeight = 200;
+    const spacing = 100;
 
     // ! Type problem
     // const group = graphRef.current.append('g')
 
     useEffect(() => {
-        // Calories bar
-        // select(graphRef.current)
-        // .selectAll('rect')
-        // .data(MOCKED_DATA[0].activityInfo.dailyActivity)
-        // .enter()
-        // .append('rect')
-        // .attr('width', 45)
-        // .attr('height', (d) => d.calories )
-        // .attr("x", (d, i) => i * 60)
-        // .attr("y", (d) => 500 - d.calories)
-
-        // Calories bar with g
         const graph = select(graphRef.current);
-        const group = graph.append('g');
-        const bars = group
+        // const group = graph.append('g');
+
+        // Calories bar
+        graph
+            .append('g')
             .selectAll('rect')
             .data(MOCKED_DATA[0].activityInfo.dailyActivity)
-            .enter()
-            .append('rect')
-            .attr('x', (d, i) => i * 60)
+            .join('rect')
+            .attr('x', (d, i) => i * spacing)
             .attr('y', (d) => svgHeight - d.calories)
             .attr('width', 40)
             .attr('height', (d) => d.calories)
             .attr('fill', '#ff0000')
             .attr('stroke', '#000000')
             .attr('stroke-width', 1)
-            .attr('fill-opacity', 0.5)
-            .attr('stroke-opacity', 0.5);
+            .attr('rx', 5);
+        // .attr('fill-opacity', 0.5)
+        // .attr('stroke-opacity', 0.5)
 
         // Weight bar
-        // ! Can't do selectAll again
-        // select(graphRef.current)
-        // .selectAll('rect')
-        // .data(MOCKED_DATA[0].activityInfo.dailyActivity)
-        // .enter()
-        // .append('rect')
-        // .attr('width', 45)
-        // .attr('height', (d) => d.weight )
-        // .attr("x", (d, i) => i * 60)
-        // .attr("y", (d) => 500 - d.weight)
+        graph
+            .append('g')
+            .selectAll('rect')
+            .data(MOCKED_DATA[0].activityInfo.dailyActivity)
+            .join('rect')
+            .attr('x', (d, i) => i * spacing)
+            .attr('y', (d) => svgHeight - d.weight)
+            .attr('width', 40)
+            .attr('height', (d) => d.weight)
+            .attr('transform', 'translate(20, 0)')
+            .attr('fill', 'blue')
+            .attr('stroke', 'blue')
+            .attr('stroke-width', 1)
+            .attr('rx', 5);
+        // .attr('fill-opacity', 0.5)
+        // .attr('stroke-opacity', 0.5)
 
         // Day element
-        select(graphRef.current)
+        graph
+            .append('g')
             .selectAll('text')
             .data(MOCKED_DATA[0].activityInfo.dailyActivity)
-            .enter()
-            .append('text')
+            .join('text')
             .text((d) => `${d.day}`)
-            .attr('x', (d, i) => i * 60)
+            .attr('x', (d, i) => i * spacing)
             .attr('y', (d) => svgHeight - d.calories)
             .style('fill', 'black');
     }, []);
-
-    // Test
-    // useEffect(() => {
-    //     const test = d3.select(testRef.current)
-    //     test.selectAll("circle")
-    //       .data(MOCKED_DATA[0].activityInfo.dailyActivity)
-    //       .join("circle")
-    //         .attr("cx", d => d[0])
-    //         .attr("cy", d => d[1])
-    //         .attr("r",  3)
-    //   }, [MOCKED_DATA[0].activityInfo.dailyActivity])
 
     return (
         <div>
