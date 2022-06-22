e1b53b309ecb6c3e7a346018749215005687a481
Léonard Wojcik <leonardw@tutanota.com>
2022-06-22 10:43:08 +0200

    feat: add style to activity graph.

--- src/components/Graphs/ActivityGraph/ActivityGraph.tsx
+++ src/components/Graphs/ActivityGraph/ActivityGraph.tsx
@@ -1,4 +1,13 @@
-import { BarChart, Bar, ResponsiveContainer } from 'recharts';
+import {
+    BarChart,
+    Bar,
+    ResponsiveContainer,
+    YAxis,
+    XAxis,
+    CartesianGrid,
+    Legend,
+    Tooltip,
+} from 'recharts';
 import { MOCKED_DATA } from '../../../helpers/MOCKED_DATA';
 
 function ActivityGraph() {
@@ -9,8 +18,32 @@ function ActivityGraph() {
                 height={300}
                 data={MOCKED_DATA[0].activityInfo.dailyActivity}
             >
-                <Bar type="monotone" dataKey="weight" fill="red" />
-                <Bar type="monotone" dataKey="calories" fill="gray" />
+                <Tooltip />
+                <Bar
+                    name="Poids (kg)"
+                    type="monotone"
+                    dataKey="weight"
+                    fill="#282D30"
+                    barSize={12}
+                    radius={[3, 3, 0, 0]}
+                />
+                <Bar
+                    name="Calories brûlées (kCal)"
+                    type="monotone"
+                    dataKey="calories"
+                    fill="#E60000"
+                    barSize={12}
+                    radius={[3, 3, 0, 0]}
+                />
+                <YAxis orientation="right" tickLine={false} axisLine={false} />
+                <XAxis dataKey="day" tickLine={false} />
+                <CartesianGrid strokeDasharray="4" vertical={false} />
+                <Legend
+                    align="right"
+                    verticalAlign="top"
+                    height={36}
+                    iconType="circle"
+                />
             </BarChart>
         </ResponsiveContainer>
     );
