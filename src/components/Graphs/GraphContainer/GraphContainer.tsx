b417cd8f29962d84b480957341ef4b4422bfac00
Léonard Wojcik <leonardw@tutanota.com>
2022-07-05 19:36:11 +0200

    perf: make resizing of the graphs more performant - Use two wrappers in order to make the resizing of the graphs more performant.

--- src/components/Graphs/GraphContainer/GraphContainer.tsx
+++ src/components/Graphs/GraphContainer/GraphContainer.tsx
@@ -6,7 +6,30 @@ type GraphContainerProps = {
 };
 
 function GraphContainer({ GraphElement, cssClasses }: GraphContainerProps) {
-    return <div className={classes[`${cssClasses}`]}>{GraphElement}</div>;
+    return (
+        <div className={classes[`${cssClasses}`]}>
+            <div
+                style={{
+                    position: 'relative',
+                    width: '100%',
+                    paddingBottom: '250px',
+                }}
+            >
+                {/* Add wrapper in position absolute in order to make the resizing of the graph more performant. Read more on this issue : https://github.com/recharts/recharts/issues/1767 and this one : https://github.com/recharts/recharts/issues/172 */}
+                <div
+                    style={{
+                        position: 'absolute',
+                        left: 0,
+                        right: 0,
+                        bottom: 0,
+                        top: 0,
+                    }}
+                >
+                    {GraphElement}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default GraphContainer;
