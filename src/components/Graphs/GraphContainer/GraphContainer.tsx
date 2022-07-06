c8134b6b7e3e554261e5d33055c8091306555a51
Léonard Wojcik <leonardw@tutanota.com>
2022-07-06 11:33:18 +0200

    fix: adjust height and width of graph elements

--- src/components/Graphs/GraphContainer/GraphContainer.tsx
+++ src/components/Graphs/GraphContainer/GraphContainer.tsx
@@ -7,26 +7,23 @@ type GraphContainerProps = {
 
 function GraphContainer({ GraphElement, cssClasses }: GraphContainerProps) {
     return (
-        <div className={classes[`${cssClasses}`]}>
+        <div
+            className={classes[`${cssClasses}`]}
+            style={{
+                position: 'relative',
+            }}
+        >
+            {/* Add wrapper in position absolute in order to make the resizing of the graph more performant. Read more on this issue : https://github.com/recharts/recharts/issues/1767 and this one : https://github.com/recharts/recharts/issues/172 */}
             <div
                 style={{
-                    position: 'relative',
-                    width: '100%',
-                    paddingBottom: '250px',
+                    position: 'absolute',
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    top: 0,
                 }}
             >
-                {/* Add wrapper in position absolute in order to make the resizing of the graph more performant. Read more on this issue : https://github.com/recharts/recharts/issues/1767 and this one : https://github.com/recharts/recharts/issues/172 */}
-                <div
-                    style={{
-                        position: 'absolute',
-                        left: 0,
-                        right: 0,
-                        bottom: 0,
-                        top: 0,
-                    }}
-                >
-                    {GraphElement}
-                </div>
+                {GraphElement}
             </div>
         </div>
     );
